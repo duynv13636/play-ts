69c8493614b9f6ca52a569a07bbc84974ae3b745
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-12-01 11:50:14 +0700

    update navbar and edit calulator

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -3,24 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent {
-  toShow='0';
-  currvalue='';
-  updateCal(value:string){
-      this.currvalue= this.currvalue+value;
-      this.toShow=this.currvalue;
+  toShow = '0';
+  currvalue = '';
+  updateCal(value: string) {
+    this.currvalue = this.currvalue + value;
+    this.toShow = this.currvalue;
   }
-  result(){   
-    console.log('curr',this.currvalue);
-    this.toShow=eval(this.currvalue);
-    this.currvalue=this.toShow;
- 
-    
+  result() {
+    // console.log('curr',this.currvalue);
+    try {
+      this.toShow = eval(this.currvalue);
+      this.currvalue = this.toShow;
+    } catch (error) {
+      this.toShow = 'error';
+    }
   }
-  clear(){
-    this.currvalue='';
-    this.toShow='0';
+  clear() {
+    this.currvalue = '';
+    this.toShow = '0';
   }
 }

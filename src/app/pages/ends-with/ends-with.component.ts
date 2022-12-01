e09d9c94caac7e3cefa818a179bc2e3d43ee27e8
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-12-01 10:16:03 +0700

    edit

--- src/app/pages/ends-with/ends-with.component.ts
+++ src/app/pages/ends-with/ends-with.component.ts
@@ -7,17 +7,26 @@ import { endWith } from 'rxjs';
   styleUrls: ['./ends-with.component.css'],
 })
 export class EndsWithComponent implements OnInit {
+  text1 = '';
+  text2 = '';
+  text3: any;
+  check = '';
   constructor() {}
-  
+
   ngOnInit(): void {
-    console.log(this.handleEndWith('duy', 'd', 1));
-    this.handleEndWith('duy', 'd');
+    // console.log(this.handleEndWith('duy', 'd', 1));
+    // this.handleEndWith('duy', 'd');
   }
-  handleEndWith(a: string, b: string, c?: number) {
+  handleEndWith(a: string, b: string, c?: any) {
     if (a.endsWith(b) || a.endsWith(b, c)) {
-      return true;
+      // return true;
+      this.check = 'true';
     } else {
-      return false;
+      this.check = 'false';
+      // return false;
     }
+  }
+  click() {
+    console.log(this.handleEndWith(this.text1, this.text2, this.text3));
   }
 }
